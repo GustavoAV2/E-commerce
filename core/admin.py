01c026e8feaf6903ceec886b60ddacf09184c8fe
Gustavo Antunes Voltolini <gustavo.voltolini@hbsis.com.br>
2021-04-28 21:38:55 -0300

    Created administration  models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from models import User, Product
+from .models import User, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "amount")
+
 
 admin.site.register(User)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
--- core/models.py
+++ core/models.py
@@ -6,9 +6,22 @@ class Product(models.Model):
     price = models.DecimalField("price", decimal_places=2, max_digits=6)
     amount = models.IntegerField("amount")
 
+    def serialize(self):
+        return {
+            "name": self.name,
+            "email": self.price,
+            "password": self.amount,
+        }
+
 
 class User(models.Model):
     name = models.CharField("name", max_length=100)
     email = models.EmailField("e-mail", max_length=100)
     password = models.CharField("password", max_length=100)
 
+    def serialize(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+        }
