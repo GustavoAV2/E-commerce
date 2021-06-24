da4a1a4014ebe2708a4e1f3c215aa1b559b8dc05
Gustavo Antunes Voltolini <gustavo.voltolini@hbsis.com.br>
2021-06-24 19:50:41 -0300

    Correcting templates

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,9 @@ from core.views import about, collection, product, manager_historic, payment_pro
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    # path('about', about, name="about"),
-    # path('collection', collection, name="collection"),
+    path('about', about, name="about"),
+    path('collection', collection, name="collection"),
     path('product/<int:product_id>', product, name="product"),
-    # path('manager/historic', manager_historic, name="manager historic"),
-    # path('payment/<int:product_id>', payment_product, name="payment_product")
+    path('manager/historic', manager_historic, name="manager historic"),
+    path('payment/<int:product_id>', payment_product, name="payment_product")
 ]
